537de47065adc776a2fb49f27b11510af4a87a92
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2022-10-26 16:19:03 +0200

    Update database_credential.php

--- Model/database_credential.php
+++ Model/database_credential.php
@@ -1,7 +1,7 @@
 <?php
 
-define("servename","localhost");
-define("username","root");
-define("password","");
-define("db","class_contact_mgt");
+define("servename","us-cdbr-east-06.cleardb.net");
+define("username","bb2644d05887cc");
+define("password","1855f1ec");
+define("db","heroku_25e4815c793ac8c");
 ?>
